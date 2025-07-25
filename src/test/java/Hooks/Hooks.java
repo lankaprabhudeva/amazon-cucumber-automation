e9b915f55a234d1d815932c4fe72d1e373e8aff5
prabhudeva <prabhudevalanka@gmail.com>
2025-07-25 18:47:55 +0530

    Initial commit

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -3,16 +3,42 @@ package Hooks;
 import io.cucumber.java.Before;
 import io.cucumber.java.After;
 import utils.DriverFactory;
+import org.testng.Reporter;
 
 public class Hooks {
 
     @Before
     public void setUp() {
-        DriverFactory.getDriver().get("https://www.primevideo.com/region/eu/storefront/ref=atv_dp_mv_c_9zZ8D2_1_2?merchId=RentBuy&ie=UTF8");
+        // Get browser from TestNG XML parameter or default to Chrome
+        String browser = "chrome";
+        try {
+            browser = Reporter.getCurrentTestResult()
+                              .getTestContext()
+                              .getCurrentXmlTest()
+                              .getParameter("browser");
+        } catch (Exception e) {
+            System.out.println("[WARN] Could not fetch browser parameter. Defaulting to Chrome.");
+        }
+
+        if (browser == null || browser.isEmpty()) {
+            browser = "chrome";
+        }
+
+        System.out.println("[INFO] Launching browser: " + browser);
+
+        // Initialize driver
+        DriverFactory.initDriver(browser);
+
+        // Get target URL from system property or use default
+        String url = System.getProperty("app.url", "https://www.primevideo.com/offers/nonprimehomepage/ref=atv_auth_signout");
+        System.out.println("[INFO] Navigating to URL: " + url);
+
+        DriverFactory.getDriver().get(url);
     }
 
     @After
     public void tearDown() {
+        System.out.println("[INFO] Tearing down WebDriver");
         DriverFactory.quitDriver();
     }
 }
