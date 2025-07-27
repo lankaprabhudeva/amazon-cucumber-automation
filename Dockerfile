3015a30b48946f943f2836230e12f7872de2fd2e
Prabhudeva <prabhudeva.lsndc@gmail.com>
2025-07-27 19:12:53 +0530

    Updated Dockerfile, pom.xml, TestRunner; removed Jenkinsfile.txt and testng.xml

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,4 @@
-# Stage 1: Build the project with tests
-FROM maven:3.9.4-eclipse-temurin-17 AS build
+FROM maven:3.9.4-eclipse-temurin-17
 WORKDIR /app
 COPY . .
-RUN mvn clean test
-
-# Optional: You can use a lightweight image for logs/output
-# But for simplicity, we just stay in the same layer and print logs directly
+CMD ["mvn", "clean", "test"]
--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,20 +1,30 @@
 package runner;
 
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.DataProvider;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features/prime.feature",  
-    glue = {"Stepdefpack", "Hooks"},
+		features = {
+				
+				"src/test/resources/features/prime.feature"
+		        
+		    },
+    glue = {"Stepdefpack", "Hooks"},          
     plugin = {
         "pretty",
         "html:target/cucumber-reports.html",
         "json:target/cucumber.json"
+        
     },
     monochrome = true,
     dryRun = false
 )
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = false) // Set true for parallel execution
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
